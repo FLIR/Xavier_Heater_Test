db15de58d5da2c52df852024cec74d4800462c01
Anil Dhurjaty <adhurjaty@novacoast.com>
2020-03-25 15:56:09 -0700

    starts on UI

--- boson_video.py
+++ boson_video.py
@@ -4,17 +4,26 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-vid = cv2.VideoCapture(0)
-vid.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'RGGB'))
-vid.set(cv2.CAP_PROP_CONVERT_RGB, False)
 
-while vid.isOpened():
-    empty, frame = vid.read()
+class BosonCamera:
+    stop_signal = False
 
-    cv2.imshow("s", frame)
+    def start(self, on_new_frame):
+        stop_signal = False
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        vid = cv2.VideoCapture(0)
+        vid.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'RGGB'))
+        vid.set(cv2.CAP_PROP_CONVERT_RGB, False)
 
-vid.release()
-print('done')
+        while vid.isOpened() and not self.stop_signal:
+            empty, frame = vid.read()
+
+            cv2.imshow("s", frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        vid.release()
+
+    def stop(self):
+        stop_signal = True
---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from PyQt5.QtWidgets import *
+
+app = QApplication([])
+win = QMainWindow()
+button = QPushButton('Test')
+win.setCentralWidget(button)
+win.show()
+app.exit(app.exec_())
+
